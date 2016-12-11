0207cc6c81962ecf3297e792f3d65ab0af40e597
Michael Banks <banks1359@gmail.com>
2016-12-10 18:23:26 -0600

    Initial Commit
    This should be interesting. Let the Party & Bugs begin…

---
+++ AbuseIPDB.py
@@ -0,0 +1,93 @@
+#! /bin/python/env
+import codecs
+import argparse
+import re
+import sys
+import requests
+from sys import argv
+
+api_key = 'YOUR_API_KEY_HERE'
+
+
+def get_file(infile):
+    with codecs.open(infile, "r", encoding='utf-8', errors='ignore') as f:
+        return f.read()
+
+
+def get_cat(x):
+    return {
+        3: 'Frad_Orders',
+        4: 'DDoS_Attack',
+        9: 'Open_Proxy',
+        10: 'Web_Spam',
+        11: 'Email_Spam',
+        14: 'Port_Scan',
+        18: 'Brute_Force',
+        19: 'Bad_Web_Bot',
+        20: 'Exploited_Host',
+        21: 'Web_App_Attack',
+        22: 'SSH',
+        23: 'IoT_Targeted',
+    }.get(x)
+
+
+def get_report(IP):
+    request = 'https://www.abuseipdb.com/check/%s/json?key=%s' % (IP, api_key)
+    # DEBUG
+    # print(request)
+    r = requests.get(request)
+    # DEBUG
+    # print(r.json())
+    try:
+        data = r.json()
+        if data == []:
+            print("%s:  No Abuse Reports" % IP)
+        else:
+            for record in data:
+                log = []
+                ip_address = ("Alert for %s:" % IP)
+                # ip_address = record['ip']
+                country = record['country']
+                # iso_code = record['isoCode']
+                category = record['category']
+                created = record['created']
+                log.append(ip_address)
+                log.append(country)
+                # log.append(iso_code)
+                log.append(created)
+                for cat in category:
+                    log.append(get_cat(cat))
+                print('\t'.join(map(str, log)))
+    except (ValueError, KeyError, TypeError):
+        print("JSON format error")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='This program utilizes the Abuse IP Database from: AbuseIPDB.com'
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        help="parses IP Addresses from a single given file",
+        action="store")
+
+    args = parser.parse_args()
+
+    if len(sys.argv) < 2:
+        parser.print_help()
+
+    if args.file:
+        f = get_file(argv[2])
+        found = re.findall(
+            r'(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})', f)
+
+        list(set(found))
+
+        print("Abuse IP Database REPORT:")
+        for ip in found:
+            get_report(ip)
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,35 @@
+# AbuseIP DB Scanner
+
+This is a python script that will parse IP addresses from files and interacts with AbuseIPDB API and return the information about the IP into standard out in tab separated values in standard out.
+
+## Requirements (Setup)
+==================
+- Python3 (2.7 may have errors)
+- Requests
+```
+pip3 install Requests
+```
+- Requests[security]
+```
+pip3 install requests[security]
+```
+- AbuseIP DB API Key
+In order to use the script you will need an API key and place it in the scrip under the "api_key" variable
+
+## Usage
+==================
+```
+python3 AbuseIPDB.py -f file_to_parse.txt
+```
+
+## Troubleshooting
+==================
+- If you are receiving errors, please look at the Issues queue and see if there is already an issue open.
+
+- If you have a unique issue, please create a new Issue, and include the output of your terminal from the bootstrap script down until the error.
+
+## To-Do (Future)
+==================
+- Parse recursively through directories
+- Create an option to output data in TSV or CSV
+- Create an option to output data in JSON
